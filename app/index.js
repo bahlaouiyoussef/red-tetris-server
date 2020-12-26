be476248659287a04cae349b74a132e653765180
Youssef Bahlaoui <youssef-bahlaoui@outlook.fr>
2020-12-26 09:18:03 +0100

    Testing sockets connection

--- app/index.js
+++ app/index.js
@@ -5,8 +5,13 @@ if (process.env.NODE_ENV == "development") {
   require("dotenv").config();
 }
 
+io.use(function auth(socket, next) {
+  next();
+});
+
 io.on("connection", function onConnection(socket) {
-  console.log("client connected")
+  console.log(`Client connected ${socket.id}`, socket.handshake.headers);
+  socket.emit("message", "hello from server");
 });
 
 module.exports = io;
